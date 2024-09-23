bf0afc670452e14e343d8ced70ca263f38e1e9f5
Nikhil <nikhilraikwar099@gmail.com>
2024-09-23 23:45:29 +0530

    new todo app

---
+++ src.js
@@ -0,0 +1,66 @@
+const input = document.querySelector(".task");
+const ad = document.querySelector(".adding");
+const display = document.querySelector(".display");
+const i = '<i class="fa-solid fa-trash"></i>';
+const def = document.querySelector(".default");
+
+ad.addEventListener("click", (e)=>{
+  def.remove();
+
+  let div = document.createElement("div");
+  div.className = "todos";
+
+  let p = document.createElement("p");
+  p.className = "txt";
+  p.innerText = input.value;
+  p.style.textTransform = 'capitalize'
+
+  let ops = document.createElement("div");
+  ops.className = "ops";
+
+  let check = document.createElement("input");
+  check.type = "checkbox";
+  check.style.cursor = "pointer";
+
+  let remove = document.createElement("button");
+  remove.className = "remove";
+  remove.innerHTML = i;
+
+  display.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(ops);
+  ops.appendChild(check);
+  ops.appendChild(remove);
+
+  input.value = ''
+
+
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      p.style.textDecoration = "line-through";
+      p.style.textDecorationColor = "red"
+    } else {
+      p.style.textDecoration = "none"
+    }
+  });
+
+
+  remove.addEventListener('click', function(e) {
+    const pa = e.target.parentElement.parentElement.parentElement
+    pa.remove()
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+  
+
+});
